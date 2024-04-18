eb4768a826e211701ea17185ac3f755fc6da4f6b
bjsneto <bjsnetinho@gmail.com>
2024-04-18 19:30:26 -0300

    remove dockerize

--- goapp/init.sh
+++ goapp/init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-dockerize -wait tcp://mysql:3306,tcp://kafka:9092 -timeout 30s
+dockerize -wait tcp://mysql:3306 -timeout 30s
 
 migrate -path /app/internal/database/migrations -database "mysql://root:root@tcp(mysql:3306)/wallet?charset=utf8&parseTime=True&loc=Local" up
